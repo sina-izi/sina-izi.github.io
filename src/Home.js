3d0be16ce169fcdb4b9c6d5298e9d8f0a6783b7e
sina-izi <113315536+sina-izi@users.noreply.github.com>
2024-09-10 22:23:07 -0400

    Created Home.js

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a brief explanation about who I am, my background, and what I do.</p>
+    </div>
+  );
+}
+
+export default Home;
