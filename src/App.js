3e1b416b02e7255f23d48ea1c4570dc2d7c865c3
sina-izi <sfa7621@nyu.edu>
2024-09-10 22:21:26 -0400

    added different sections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // You can customize the CSS for styling
+import Home from './Home';
+import Resume from './Resume';
+import Blog from './Blog';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#resume">Resume</a></li>
+            <li><a href="#blog">Blog</a></li>
+          </ul>
+        </nav>
       </header>
+      <section id="home">
+        <Home />
+      </section>
+      <section id="resume">
+        <Resume />
+      </section>
+      <section id="blog">
+        <Blog />
+      </section>
     </div>
   );
 }
