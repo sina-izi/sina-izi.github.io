f230b804ef54be74b5b650f8eb6d0bfc2599a3f8
sina-izi <sfa7621@nyu.edu>
2024-09-19 13:58:13 -0400

    multiple page added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import './App.css'; // You can customize the CSS for styling
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
 import Home from './Home';
 import Resume from './Resume';
 import Blog from './Blog';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <nav>
-          <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#resume">Resume</a></li>
-            <li><a href="#blog">Blog</a></li>
-          </ul>
-        </nav>
-      </header>
-      <section id="home">
-        <Home />
-      </section>
-      <section id="resume">
-        <Resume />
-      </section>
-      <section id="blog">
-        <Blog />
-      </section>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/resume">Resume</Link></li>
+              <li><Link to="/blog">Blog</Link></li>
+            </ul>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </div>  {/* Closing div tag correctly */}
+    </Router>
   );
 }
 
