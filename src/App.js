1508937ae2cc05990d7c86ba1917dde7966d0b3b
sina-izi <sfa7621@nyu.edu>
2024-09-19 22:18:43 -0400

    added light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
@@ -6,14 +6,23 @@ import Resume from './Resume';
 import Blog from './Blog';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
     <Router>
-      <div className="App">
+      <div className={`App ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
         <header className="App-header">
           <nav className="nav-buttons">
             <Link className="button" to="/">Home</Link>
             <Link className="button" to="/resume">Resume</Link>
             <Link className="button" to="/blog">Blog</Link>
+            <button className="button theme-toggle" onClick={toggleTheme}>
+              {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+            </button>
           </nav>
         </header>
 
