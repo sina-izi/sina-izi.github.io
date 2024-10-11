b40c4c3e37293aea79813ec23d941f6371799efb
sina-izi <sfa7621@nyu.edu>
2024-10-11 19:41:08 -0400

    change of intro

--- src/Home.js
+++ src/Home.js
@@ -15,20 +15,13 @@ function Home() {
             className="about-picture"
           />
           <p className="about-text">
-            Hi! I'm Sina, I am a Developer who studied Computer Engineering at NYU.
-            I think I almost maxed out these stats:<br /><br />
-            &nbsp;&nbsp;&nbsp;&nbsp;- Extrovertedness: I love making new friends and 
-            hanging out with them!<br /><br />
-            &nbsp;&nbsp;&nbsp;&nbsp;- Creativity: I'm really good at coming up with 
-            original creative Ideas.<br /><br />
-            &nbsp;&nbsp;&nbsp;&nbsp;- Adaptability: I don't fear change, I am change!
-             But seriously! I always love to challenge how I do things and try new 
-             ways.<br /><br />
-            However, I'm on a quest to level up the stats below. I'd say that they are
-             a work in progress :D<br /><br />
-             &nbsp;&nbsp;&nbsp;&nbsp;- Proactiveness: Sometime i just need to take the
-              step. Enough with the ideas :/<br /><br />
-             &nbsp;&nbsp;&nbsp;&nbsp;- Experience: I do want to learn more and become a way better Engineer.<br /><br />
+            Hi! I'm Sina, I am a Developer who studied Computer Engineering at NYU. I think I almost maxed out these stats:<br /><br />
+            &nbsp;&nbsp;&nbsp;&nbsp;- Extroversion: I love making new friends and hanging out with them!<br /><br />
+            &nbsp;&nbsp;&nbsp;&nbsp;- Creativity: I'm really good at coming up with creative Ideas. I consider this to be my superpower!<br /><br />
+            &nbsp;&nbsp;&nbsp;&nbsp;- Adaptability: I am change! No but seriously! I love to be challenged and try new things.<br /><br />
+            However, I'm on a quest to level up the stats below. I'd say that they are a work in progress :D<br /><br />
+             &nbsp;&nbsp;&nbsp;&nbsp;- Proactiveness: Sometime i just need to take the step. Enough with the ideas :/<br /><br />
+             &nbsp;&nbsp;&nbsp;&nbsp;- Experience: I do want to learn more and become a way better Engineer, and a better Leader.<br /><br />
              &nbsp;&nbsp;&nbsp;&nbsp;- Story-Telling: I guess we all know the power of great stories! and I'm working on mine.<br />
           </p>
         </div>
