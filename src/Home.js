9e781a33839cc2c4d8fbc1356db30fcf7af3cfc1
sina-izi <sfa7621@nyu.edu>
2024-11-12 22:35:01 -0500

    added typewriting animation

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './Home.css'; // Create a Home.css file for styling
+import './Home.css';
+import aboutText from 'texts/AboutText';
 
 function Home() {
   return (
     <div className="home-container">
       <section className="about-me">
-        <h2>Me?</h2>
+        <h2 className="typewriter">Hi I'm Sina!</h2>
 
         {/* Picture and Text */}
         <div className="about-content">
@@ -15,14 +16,7 @@ function Home() {
             className="about-picture"
           />
           <p className="about-text">
-            Hi! I'm Sina, I am a Developer who studied Computer Engineering at NYU. I think I almost maxed out these stats:<br /><br />
-            &nbsp;&nbsp;&nbsp;&nbsp;- Extroversion: I love making new friends and hanging out with them!<br /><br />
-            &nbsp;&nbsp;&nbsp;&nbsp;- Creativity: I'm really good at coming up with creative Ideas. I consider this to be my superpower!<br /><br />
-            &nbsp;&nbsp;&nbsp;&nbsp;- Adaptability: I am change! No but seriously! I love to be challenged and try new things.<br /><br />
-            However, I'm on a quest to level up the stats below. I'd say that they are a work in progress :D<br /><br />
-             &nbsp;&nbsp;&nbsp;&nbsp;- Proactiveness: Sometime i just need to take the step. Enough with the ideas :/<br /><br />
-             &nbsp;&nbsp;&nbsp;&nbsp;- Experience: I do want to learn more and become a way better Engineer, and a better Leader.<br /><br />
-             &nbsp;&nbsp;&nbsp;&nbsp;- Story-Telling: I guess we all know the power of great stories! and I'm working on mine.<br />
+            {aboutText}
           </p>
         </div>
       </section>
---
+++ texts/AboutText.js
@@ -0,0 +1,21 @@
+// AboutText.js
+
+const aboutText = `
+  Hi! I'm Sina, I am a Developer who studied Computer Engineering at NYU. I think I almost maxed out these stats:
+
+  - Extroversion: I love making new friends and hanging out with them!
+
+  - Creativity: I'm really good at coming up with creative ideas. I consider this to be my superpower!
+
+  - Adaptability: I am change! No but seriously! I love to be challenged and try new things.
+
+  However, I'm on a quest to level up the stats below. I'd say that they are a work in progress :D
+
+  - Proactiveness: Sometimes I just need to take the step. Enough with the ideas :/
+
+  - Experience: I do want to learn more and become a way better Engineer, and a better Leader.
+
+  - Story-Telling: I guess we all know the power of great stories! and I'm working on mine.
+`;
+
+export default aboutText;
