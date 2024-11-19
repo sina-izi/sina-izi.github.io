cbe4cc8baab139175a18246a1bba4279217a002f
sina-izi <sinafallah98@gmail.com>
2024-11-19 17:10:19 -0500

    added delay to home background animation for better mobile experience

--- src/components/InfiniteCorridor.js
+++ src/components/InfiniteCorridor.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './InfiniteCorridor.css'; // Ensure the CSS file is correctly imported
 
 const InfiniteCorridor = () => {
+  const [isInitialLoad, setIsInitialLoad] = useState(true);
+
+  useEffect(() => {
+    // Add a delay for the first load
+    const timer = setTimeout(() => {
+      setIsInitialLoad(false);
+    }, 2200); // Adjust delay time in milliseconds (1000ms = 1s)
+
+    return () => clearTimeout(timer); // Cleanup on component unmount
+  }, []);
+
   return (
     <div className="infinite-corridor">
       <div className="perspective">
         <div className="slider">
           {[...Array(5)].map((_, index) => (
-            <div className="corridor" key={index}>
+            <div
+              className={`corridor ${isInitialLoad ? 'initial-load' : ''}`}
+              key={index}
+            >
               <div></div>
               <div></div>
               <div></div>
