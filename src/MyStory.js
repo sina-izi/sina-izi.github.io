98bf9a22101c21865f6172bc9b6e488dfd489ceb
sina-izi <sinafallah98@gmail.com>
2024-12-02 15:41:48 -0500

    updated the content in story

--- src/MyStory.js
+++ src/MyStory.js
@@ -4,67 +4,63 @@ import './MyStory.css';
 const timelineData = [
   {
     year: 2019,
-    title: "2019 - Flight sweat and tears",
-    text: `I still remember the last goodbye. It was heartbreaking but full of hope.
-    Hope for a better future, a better life, and an uncertain reunion. As I was moving 
-    towards the airport TTC, I could see their warm smiles and teary eyes fade away.
-    I left them behind. I left my parents, friends, and everyone I cared for behind.
-    So that I can pursue my own American dream, hoping that one day I will see them 
-    in better circumstances. But as much as I was sad, I was excited. Not only was I 
-    going to the land of opportunities, the United States of America, but also I was 
-    going to the capital of the world, the big apple, the city that never sleeps, NYC. 
-    This was my dream city since I was a kid. So I would be lying if I said I was not 
-    super excited to finally be able to go there, especially knowing that I got rejected 
-    3 times before I finally got accepted! This was a great opportunity for me to finally 
-    grow up, learn to become independent, and build a great future for myself and those 
-    who love me.`,
-    image: "path_to_image_2019.jpg",
+    title: "2019 Naive Yet Confident (NYC)",
+    text: `My journey started here when I came to the United States to chase my own 
+    American dream. Of course, it wasn’t going to be a smooth ride all the way. After all, 
+    this was my first time being away from my family. Back home, I was a spoiled kid who 
+    had never opened a bank account or worried much about spending or planning. Being 
+    away from home meant that I had to learn to become independent.`,
+    image: "https://drive.google.com/file/d/1YcF6JCd7DTSF3Jegj-hROS-Vjo6bBz6s/view?usp=share_link",
   },
   {
     year: 2020,
-    title: "2020 - The great reset",
-    text: `I still remember the last goodbye. It was heartbreaking but full of hope.
-    Hope for a better future, a better life, and an uncertain reunion. As I was moving 
-    towards the airport TTC, I could see their warm smiles and teary eyes fade away.
-    I left them behind. I left my parents, friends, and everyone I cared for behind.
-    So that I can pursue my own American dream, hoping that one day I will see them 
-    in better circumstances. But as much as I was sad, I was excited. Not only was I 
-    going to the land of opportunities, the United States of America, but also I was 
-    going to the capital of the world, the big apple, the city that never sleeps, NYC. 
-    This was my dream city since I was a kid. So I would be lying if I said I was not 
-    super excited to finally be able to go there, especially knowing that I got rejected 
-    3 times before I finally got accepted! This was a great opportunity for me to finally 
-    grow up, learn to become independent, and build a great future for myself and those 
-    who love me.`,
+    title: "2020 The great reset",
+    text: `This was the year I got into NYU. Now, I had a big name behind me that 
+    opened many new doors. It also gave me the much-needed confidence to believe in 
+    myself. However, there was one small problem—the pandemic! No fun events or 
+    friendships at school. There was another issue: I couldn’t make any friends 
+    because of COVID policies. This resulted in a lonely period in my life.`,
     image: "path_to_image_2020.jpg",
   },
   {
     year: 2021,
-    title: "2021 - Friends or not?",
-    text: "This is the description for 2021. Major events of the year.",
+    title: "2021 Friends or not?",
+    text: `As a result of COVID, I didn’t have any friends at school. So, I had to 
+    face the fear of talking to strangers. That’s when I had a conversation with a 
+    classmate who inspired me to set a challenge: talk to a new stranger every day 
+    for a couple of months. So, that’s what I did! It helped me become better at 
+    finding common interests and steering the conversation in that direction. But 
+    believe me, there were plenty of failed attempts along the way to get to that 
+    point!`,
     image: "path_to_image_2021.jpg",
   },
   {
     year: 2022,
-    title: "2022 - Measure",
-    text: "This is the description for 2022. Significant milestones.",
+    title: "2022 Measure",
+    text: `During the cold January, while walking, I was listening to an inspiring 
+    podcast about measurement. That’s when I realized that if I wanted to improve 
+    something, I needed to measure it first. So, I began tracking how I spent my 
+    time and money by creating a customized spreadsheet for myself. I also took 
+    inspiration from game psychology and developed an automatic ranking system and 
+    visual stats, like graphs, to make the process more fun and engaging. After all, 
+    why shouldn’t I use those techniques for a good addiction?`,
     image: "path_to_image_2022.jpg",
   },
   {
     year: 2023,
-    title: "2023 - Refocus",
+    title: "2023 Refocus",
     text: "This is the description for 2023. Exciting developments.",
     image: "path_to_image_2023.jpg",
   },
   {
     year: 2024,
-    title: "2024 - Consequences",
+    title: "2024 Consequences",
     text: "This is the description for 2024. Future outlook.",
     image: "path_to_image_2024.jpg",
   },
   {
     year: 2025,
-    title: "2025 - Building my own luck",
+    title: "2025 Make my own luck",
     text: "This is the description for 2025. The road ahead.",
     image: "path_to_image_2025.jpg",
   },
